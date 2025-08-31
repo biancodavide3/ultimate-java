978d31252dd346a6bf8b6a98832520862c935bdc
Davide Bianco <davidebianco1@icloud.com>
2025-08-31 16:48:13 +0200

    deploying section

---
+++ src/main/java/com/biancodavide3/java/foundamentals/deploying/Main.java
@@ -0,0 +1,14 @@
+package com.biancodavide3.java.foundamentals.deploying;
+
+public class Main {
+    public static void main(String[] args) {
+        // errors fall into two categories:
+        // 1) syntax errors which prevent the code from even compiling
+        // 2) runtime errors which cause the code to crash or not give the expected result
+        // syntax errors can be easily fixed using while using IDE like IntelliJ IDEA
+        // runtime errors are more difficult to track down and to help we can use a debugger
+        // In the real world applications should be tested before being deployed
+        // after making sure the code is working as expected we can deploy it as a jar
+        // and distribute it
+    }
+}
